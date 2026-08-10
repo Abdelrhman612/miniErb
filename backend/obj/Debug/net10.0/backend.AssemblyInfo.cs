cc537b086e89d731adc9579caf48871f84cbb5ab
Abdelrahman <abdelrahman.ayman.tech@gmail.com>
2026-08-10 17:21:20 +0300

    chore: update assembly informational version and build cache metadata

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65079a0d2de97240882ee517a1f24e72bfda9839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdac3310c8f946205a2272ef96a94602d64679d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
